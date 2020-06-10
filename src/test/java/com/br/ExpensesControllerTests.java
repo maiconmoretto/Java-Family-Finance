d8b0da26601ae282405ac38275014afd818b3aaf
maiconmoretto <maiconmorettos@gmail.com>
2020-06-10 17:56:25 -0300

    Added test save expense in controller test

--- src/test/java/com/br/ExpensesControllerTests.java
+++ src/test/java/com/br/ExpensesControllerTests.java
@@ -1,6 +1,8 @@
 package com.br;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -9,6 +11,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import com.br.model.Expenses;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -17,10 +21,29 @@ public class ExpensesControllerTests {
 	@Autowired
 	private MockMvc mvc;
 
+
 	@Test
-	public void get() throws Exception {
+	public void findAll() throws Exception {
 		mvc.perform(MockMvcRequestBuilders.get("/api/v1/expenses/").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void save() throws Exception {
+		Expenses expense = new Expenses();
+		mvc.perform(MockMvcRequestBuilders.post("/api/v1/expenses/").content(asJsonString(expense))
+				.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON));
+
+	}
+
+	public static String asJsonString(final Object obj) {
+		try {
+			final ObjectMapper mapper = new ObjectMapper();
+			final String jsonContent = mapper.writeValueAsString(obj);
+			return jsonContent;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }

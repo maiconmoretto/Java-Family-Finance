ee957a6da1bb23252772ae9c6fc7bcc06caaf616
maiconmoretto <maiconmorettos@gmail.com>
2020-06-12 13:53:37 -0300

    Create documentation for API with swagger

--- src/main/java/com/br/controller/ExpensesController.java
+++ src/main/java/com/br/controller/ExpensesController.java
@@ -15,31 +15,40 @@ import org.springframework.web.bind.annotation.RestController;
 import com.br.model.Expenses;
 import com.br.service.ExpensesService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
+@Api(value = "API Expenses")
 public class ExpensesController {
 	@Autowired
 	private ExpensesService service;
 
+	@ApiOperation(value = "It will return list of Expenses")
 	@GetMapping(path = "/api/v1/expenses/")
 	public @ResponseBody Iterable<Expenses> getAllExpensess() {
 		return service.findAll();
 	}
 
+	@ApiOperation(value = "It will get a Expense by Id")
 	@GetMapping("/api/v1/expenses/{id}")
 	public Expenses findById(@PathVariable int id) {
 		return service.findById(id);
 	}
 
+	@ApiOperation(value = "It will add new Expense")
 	@PostMapping(path = "/api/v1/expenses/")
 	public @ResponseBody ResponseEntity save(@RequestBody Expenses expense) {
 		return service.save(expense);
 	}
 
+	@ApiOperation(value = "It will update Expense")
 	@PutMapping(value = "/api/v1/expenses/{id}")
 	public ResponseEntity<String> update(@RequestBody Expenses expense) {
 		return service.update(expense);
 	}
 
+	@ApiOperation(value = "It will delete Expense")
 	@DeleteMapping(path = "/api/v1/expenses/{id}")
 	public ResponseEntity<String> deleteById(@PathVariable int id) {
 		return service.deleteById(id);

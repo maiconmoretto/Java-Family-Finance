0506f6c9fd0e3fc35ade3b67f1f79f7f9bb9a329
maiconmoretto <maiconmorettos@gmail.com>
2020-08-29 18:28:58 -0300

    tryng to connect mongo and user

---
+++ user/src/main/java/com/br/model/User.java
@@ -0,0 +1,57 @@
+package com.br.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User implements Serializable {
+
+	@Id
+	@Column(name = "id")
+	int id;
+	@Column(name = "name")
+	String name;
+	@Column(name = "email")
+	String email;
+	@Column(name = "created_at")
+	String createdAt;
+
+	public User() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	public User(String name, String email) {
+		this.name = name;
+		this.email = email;
+	}
+}

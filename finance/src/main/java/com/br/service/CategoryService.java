49d400e939c66483d306989e9d3d4055dd86c894
maiconmoretto <maiconmorettos@gmail.com>
2020-09-03 19:15:08 -0300

    Created link between user service and finance service, now when try to add a  category in finance serve, there is a validation on user service to check if the user exist

--- finance/src/main/java/com/br/service/CategoryService.java
+++ finance/src/main/java/com/br/service/CategoryService.java
@@ -1,22 +1,43 @@
 package com.br.service;
 
+import com.br.model.UserDTO;
+import feign.FeignException;
+import com.br.user.UserServiceClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.br.model.Category;
 import com.br.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class CategoryService {
 	
 	private final CategoryRepository repository;
+
+	@Autowired
+	private UserServiceClient userServiceClient;
 	
 	@Autowired
 	public CategoryService(CategoryRepository repository) {
 		this.repository = repository;
 	}
 
-	public Category save(Category category) {
+	public Category save(Category category, int createdBy) {
+		UserDTO userDTO;
+
+		try {
+			userDTO = userServiceClient.findById(createdBy);
+		} catch (FeignException ex) {
+			if (HttpStatus.NOT_FOUND.value() == ex.status()) {
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Nao existe usuario com id: " + createdBy);
+			} else {
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Sistema indisponível no momento.");
+			}
+		}
+
 		return repository.save(category);
 	}
 

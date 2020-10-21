2d54a0fe41cd01a3afb7819b843d41d66e2f2773
maiconmoretto <maiconmorettos@gmail.com>
2020-10-21 17:52:57 -0300

    Added swagger to expense

--- expense/src/main/java/com/br/controller/ExpenseController.java
+++ expense/src/main/java/com/br/controller/ExpenseController.java
@@ -20,35 +20,44 @@ import com.br.service.ExpenseService;
 
 import java.util.List;
 import java.util.Optional;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 
 @RestController
+@Api(value = "API SharedFinance")
 @RequestMapping("expense")
 public class ExpenseController {
 
 	@Autowired
 	private ExpenseService service;
 
+	@ApiOperation(value = "It will return list of Expense")
 	@GetMapping
 	public List<Expense> viewAllExpenses() {
 		return service.viewAllExpenses();
 	}
 
+	@ApiOperation(value = "It will add new Expense")
 	@PostMapping
 	public Expense createExpense(@RequestBody Expense expense) {
 		return service.createExpense(expense);
 	}
 
+	@ApiOperation(value = "It will get a Expense by Id")
 	@GetMapping("/{id}")
 	public Expense viewExpense(@PathVariable Integer id) {
 		return service.viewExpense(id);
 	}
 
+	@ApiOperation(value = "It will update Expense")
 	@PutMapping("/{id}")
 	public Expense updateExpense(@PathVariable Integer id, @RequestBody Expense expense) {
 		expense.setId(id);
 		return service.updateExpense(expense);
 	}
 
+	@ApiOperation(value = "It will delete Expense")
 	@DeleteMapping("/{id}")
 	public void deleteExpense(@PathVariable Integer id) {
 		service.deleteExpense(id);

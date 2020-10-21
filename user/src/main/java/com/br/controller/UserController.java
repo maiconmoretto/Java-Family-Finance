086be2f755137034ebb4c1d46a14ab4a5169fc10
maiconmoretto <maiconmorettos@gmail.com>
2020-10-21 18:14:03 -0300

    Added swagger in User

--- user/src/main/java/com/br/controller/UserController.java
+++ user/src/main/java/com/br/controller/UserController.java
@@ -20,35 +20,43 @@ import com.br.service.UserService;
 
 import java.util.List;
 import java.util.Optional;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
+@Api(value = "API User")
 @RequestMapping("user")
 public class UserController {
 
 	@Autowired
 	private UserService service;
 
+	@ApiOperation(value = "It will return list of User")
 	@GetMapping
 	public List<User> viewAllUsers() {
 		return service.viewAllUsers();
 	}
 
+	@ApiOperation(value = "It will add new User")
 	@PostMapping
 	public User createUser(@RequestBody User user) {
 		return service.createUser(user);
 	}
 
+	@ApiOperation(value = "It will get a User by Id")
 	@GetMapping("/{id}")
 	public User viewUser(@PathVariable Integer id) {
 		return service.viewUser(id);
 	}
 
+	@ApiOperation(value = "It will update User")
 	@PutMapping("/{id}")
 	public User updateUser(@PathVariable Integer id, @RequestBody User user) {
 		user.setId(id);
 		return service.updateUser(user);
 	}
 
+	@ApiOperation(value = "It will delete User")
 	@DeleteMapping("/{id}")
 	public void deleteUser(@PathVariable Integer id) {
 		service.deleteUser(id);
